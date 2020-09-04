c6244b58e7b13e2944adf74d96d8ac63e43fa6b2
Naveenkumar_rdnk <52662298+naveenkumar-prog@users.noreply.github.com>
2020-09-04 08:08:59 +0530

    intial_c++programs

---
+++ display _size.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int b;
+char a;
+float c;
+double d;
+
+int main()
+{
+	cout << "Size of char\t" << sizeof(a) << "Byte" ;
+	cout << "\nSize of int\t" << sizeof(b) << "Byte" ;
+		cout << "\nSize of float\t" << sizeof(c) << "Byte" ;
+	cout << "\nSize of double\t" << sizeof(d) << "Byte" ;
+
+
+
+	return 0;
+}
---
+++ multiply.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int x;
+int y;
+
+int main()
+{
+	cout << "Welcome to the Multiplication Program!!";
+	cout << "\n\n Enter the two numbers you wish to multiply ";
+	cout << "\n Enter the first number : ";
+	cin >> x;
+	cout << "\n Enter the second number : ";
+	cin >> y;
+	
+	cout << "\n The answer is : " << x*y;
+}
